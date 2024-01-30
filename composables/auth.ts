52c98dc6649b569af314dcd3208a0a79cf999ff3
virgosoy <virgosoy@126.com>
2024-01-30 16:54:36 +0800

    refactor: 加点注释，优化下异常处理。

--- composables/auth.ts
+++ composables/auth.ts
@@ -1,5 +1,11 @@
 export const useAuth = () => useNuxtApp().$auth
 
+/**
+ * 登录 \
+ * 错误时返回 reject
+ * @param account 账号
+ * @param password 密码
+ */
 export const authLogin = async (account: string, password: string) => {
   await $fetch("/api/auth/login", {
     method: "POST",
@@ -13,6 +19,11 @@ export const authLogin = async (account: string, password: string) => {
   await navigateTo(useAuth().redirectTo.value || "/");
 };
 
+/**
+ * 注册并登录
+ * @param account 账号
+ * @param password 密码
+ */
 export const authRegister = async (account: string, password: string) => {
   await $fetch("/api/auth/register", {
     method: "POST",
@@ -24,6 +35,9 @@ export const authRegister = async (account: string, password: string) => {
   return await authLogin(account, password);
 };
 
+/**
+ * 当前用户退出登录
+ */
 export const authLogout = async () => {
   await $fetch("/api/auth/logout", {
     method: "POST",
--- server/utils/db.ts
+++ server/utils/db.ts
@@ -14,12 +14,20 @@ export interface User {
   password: string;
 }
 
-export async function findUserByAccount(account: string): Promise<User> {
+/**
+ * 根据账号查找用户
+ * @param account 
+ */
+export async function findUserByAccount(account: string): Promise<User | null> {
   const storage = useStorage();
   const key = getUserKey(account!);
   return await storage.getItem(key);
 }
 
+/**
+ * 创建用户
+ * @param user.password 是经过 hash 的，可以使用 await hash(password) 生成 
+ */
 export async function createUser(user: Partial<User>) {
   const storage = useStorage();
   const key = getUserKey(user.account!);
@@ -32,9 +40,17 @@ export async function createUser(user: Partial<User>) {
   });
 }
 
+/**
+ * 根据账号更新用户
+ * @param account 
+ * @param updates 更新的数据
+ */
 export async function updateUserByAccount(account: string, updates: Partial<User>) {
   const storage = useStorage();
   const user = await findUserByAccount(account);
+  if (!user) {
+    throw createError({ message: 'User not found!', statusCode: 404 });
+  }
   const key = getUserKey(user.account!);
   return await storage.setItem(key, {
       ...user,
